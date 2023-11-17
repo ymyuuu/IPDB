47f0806a9251a46cf167fa2e63883e8037562602
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2023-11-17 21:37:22 +0800

    Update gay.py

--- .github/workflows/gay.py
+++ .github/workflows/gay.py
@@ -66,7 +66,7 @@ if response.status_code == 200:
     for ip_address in ip_addresses:
         dns_record = {
             "type": "A",
-            "name": "ip",
+            "name": "cf",
             "content": ip_address,
             "ttl": 600,
             "proxied": False
