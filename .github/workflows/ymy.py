172b3d74f18b67c7697e44c3b892ea3e355ad682
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-01-24 21:52:28 +0800

    Update ymy.py

--- .github/workflows/ymy.py
+++ .github/workflows/ymy.py
@@ -7,34 +7,26 @@ def get_api_headers(api_token):
         "Content-Type": "application/json"
     }
 
-def get_dns_records(base_url, headers):
+def get_dns_records(base_url, headers, name):
     response = requests.get(base_url, headers=headers)
     if response.status_code == 200:
-        return response.json().get("result", [])
+        return [record for record in response.json().get("result", []) if record.get("name") == name]
     else:
-        print("YMY无法获取DNS记录信息。响应代码:", response.status_code)
+        print(f"YMY无法获取{name} DNS记录信息。响应代码:", response.status_code)
         exit(1)
 
-def delete_records_by_name(base_url, headers, name):
-    print(f"\n正在删除所有 DNS 记录，name={name}")
-    records = get_dns_records(base_url, headers)
-    if not records:
-        print(f"没有找到要删除的记录，name={name}")
-        return
-
+def delete_records(base_url, headers, records):
+    print(f"\n正在删除所有 DNS {name} 记录")
     for record in records:
-        print(f"记录详情: {record}")
-        if record["name"] == name:
-            delete_url = f"{base_url}/{record['id']}"
-            response = requests.delete(delete_url, headers=headers)
-            if response.status_code != 200:
-                print(f"YMY删除记录时出错，HTTP响应代码：{response.status_code}")
-                print(response.text)  # 打印具体错误信息
-                exit(1)
-    print(f"已删除所有 DNS 记录，name={name}")
+        delete_url = f"{base_url}/{record['id']}"
+        response = requests.delete(delete_url, headers=headers)
+        if response.status_code != 200:
+            print(f"YMY删除{name}记录时出错，HTTP响应代码：", response.status_code)
+            exit(1)
+    print(f"已删除所有{name}记录")
 
 def create_dns_records(base_url, headers, name, ip_addresses):
-    print("\n正在获取反代IP并DNS推送\n")
+    print(f"\n正在获取反代IP并{name} DNS推送\n")
     for ip_address in ip_addresses:
         dns_record = {
             "type": "A",
@@ -47,8 +39,7 @@ def create_dns_records(base_url, headers, name, ip_addresses):
         response = requests.post(base_url, headers=headers, json=dns_record)
 
         if response.status_code != 200:
-            print(f"YMY创建DNS记录时出错，HTTP响应代码：{response.status_code}")
-            print(response.text)  # 打印具体错误信息
+            print(f"YMY创建{name} DNS记录时出错，HTTP响应代码：{response.status_code}")
             exit(1)
         else:
             print(f"Successfully updated, {ip_address}")
@@ -57,12 +48,13 @@ def main():
     api_url = "https://ipdb.api.030101.xyz/?type=bestproxy"
     api_token = os.environ.get('YMYCLOUDFLARE_API_TOKEN')
     zone_id = os.environ.get('YMYZONE_ID')
-    dns_name = "0101"  # 设置要操作的 DNS 记录的 name
+    name = "0101"  # 设置DNS记录的"name"
     base_url = f"https://proxy.api.030101.xyz/https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
     headers = get_api_headers(api_token)
 
-    # 删除指定 "name" 的所有记录
-    delete_records_by_name(base_url, headers, dns_name)
+    # 获取并删除所有指定"name"的记录
+    records = get_dns_records(base_url, headers, name)
+    delete_records(base_url, headers, records)
 
     # 发送GET请求到API获取反代IP
     response = requests.get(api_url)
@@ -70,7 +62,7 @@ def main():
     # 检查反代IP请求是否成功
     if response.status_code == 200:
         ip_addresses = [ip.strip() for ip in response.text.split('\n') if ip.strip() and '.' in ip]
-        create_dns_records(base_url, headers, dns_name, ip_addresses)
+        create_dns_records(base_url, headers, name, ip_addresses)
     else:
         print("YMY无法获取反代IP地址信息。响应代码:", response.status_code)
         exit(1)
