8be2efc85f3ec0497f637e340505fc33fc4a9712
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-01-24 21:41:30 +0800

    Update ymy.py

--- .github/workflows/ymy.py
+++ .github/workflows/ymy.py
@@ -16,7 +16,7 @@ def get_dns_records(base_url, headers):
         exit(1)
 
 def delete_records_by_name(base_url, headers, name):
-    print(f"\n正在删除所有 DNS 记录中 name 为 '{name}' 的记录")
+    print(f"\n正在删除所有 DNS 记录，name={name}")
     records = get_dns_records(base_url, headers)
     for record in records:
         if record["name"] == name:
@@ -25,9 +25,9 @@ def delete_records_by_name(base_url, headers, name):
             if response.status_code != 200:
                 print(f"YMY删除记录时出错，HTTP响应代码：{response.status_code}")
                 exit(1)
-    print(f"已删除所有 DNS 记录中 name 为 '{name}' 的记录")
+    print(f"已删除所有 DNS 记录，name={name}")
 
-def create_dns_records(base_url, headers, ip_addresses, name):
+def create_dns_records(base_url, headers, name, ip_addresses):
     print("\n正在获取反代IP并DNS推送\n")
     for ip_address in ip_addresses:
         dns_record = {
@@ -50,12 +50,12 @@ def main():
     api_url = "https://ipdb.api.030101.xyz/?type=bestproxy"
     api_token = os.environ.get('YMYCLOUDFLARE_API_TOKEN')
     zone_id = os.environ.get('YMYZONE_ID')
+    dns_name = "0101"  # 设置要操作的 DNS 记录的 name
     base_url = f"https://proxy.api.030101.xyz/https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
     headers = get_api_headers(api_token)
-    name_to_delete = "0101"  # 修改为你想删除的 name 变量值
 
-    # 删除所有指定 name 的记录
-    delete_records_by_name(base_url, headers, name_to_delete)
+    # 删除指定 "name" 的所有记录
+    delete_records_by_name(base_url, headers, dns_name)
 
     # 发送GET请求到API获取反代IP
     response = requests.get(api_url)
@@ -63,7 +63,7 @@ def main():
     # 检查反代IP请求是否成功
     if response.status_code == 200:
         ip_addresses = [ip.strip() for ip in response.text.split('\n') if ip.strip() and '.' in ip]
-        create_dns_records(base_url, headers, ip_addresses, name_to_delete)
+        create_dns_records(base_url, headers, dns_name, ip_addresses)
     else:
         print("YMY无法获取反代IP地址信息。响应代码:", response.status_code)
         exit(1)
