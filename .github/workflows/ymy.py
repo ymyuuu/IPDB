41e6fe7f763bf988e8af15bcc05e5066ef113877
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-01-24 21:36:06 +0800

    Update ymy.py

--- .github/workflows/ymy.py
+++ .github/workflows/ymy.py
@@ -12,7 +12,7 @@ def get_dns_records(base_url, headers):
     if response.status_code == 200:
         return response.json().get("result", [])
     else:
-        print("YMY无法获取DNS记录信息。响应代码:", response.status_code)
+        print("无法获取DNS记录信息。响应代码:", response.status_code)
         exit(1)
 
 def delete_records_by_name(base_url, headers, name):
@@ -23,12 +23,12 @@ def delete_records_by_name(base_url, headers, name):
             delete_url = f"{base_url}/{record['id']}"
             response = requests.delete(delete_url, headers=headers)
             if response.status_code != 200:
-                print(f"YMY删除记录时出错，HTTP响应代码：{response.status_code}")
+                print(f"删除记录时出错，HTTP响应代码：{response.status_code}")
                 exit(1)
     print(f"已删除所有 DNS 记录中 name 为 '{name}' 的记录")
 
 def create_dns_records(base_url, headers, ip_addresses, name):
-    print("\n正在获取反代IP并DNS推送\n")
+    print("\n正在获取IP并DNS推送\n")
     for ip_address in ip_addresses:
         dns_record = {
             "type": "A",
@@ -41,31 +41,31 @@ def create_dns_records(base_url, headers, ip_addresses, name):
         response = requests.post(base_url, headers=headers, json=dns_record)
 
         if response.status_code != 200:
-            print(f"YMY创建DNS记录时出错，HTTP响应代码：{response.status_code}")
+            print(f"创建DNS记录时出错，HTTP响应代码：{response.status_code}")
             exit(1)
         else:
             print(f"Successfully updated, {ip_address}")
 
 def main():
     api_url = "https://ipdb.api.030101.xyz/?type=bestproxy"
-    api_token = os.environ.get('YMYCLOUDFLARE_API_TOKEN')
-    zone_id = os.environ.get('YMYZONE_ID')
+    api_token = os.environ.get('CLOUDFLARE_API_TOKEN')
+    zone_id = os.environ.get('ZONE_ID')
     base_url = f"https://proxy.api.030101.xyz/https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
     headers = get_api_headers(api_token)
-    name_to_delete = "0102"  # 修改为你想删除的 name 变量值
+    name_to_delete = "0101"
 
     # 删除所有指定 name 的记录
     delete_records_by_name(base_url, headers, name_to_delete)
 
-    # 发送GET请求到API获取反代IP
+    # 发送GET请求到API获取IP
     response = requests.get(api_url)
 
-    # 检查反代IP请求是否成功
+    # 检查IP请求是否成功
     if response.status_code == 200:
         ip_addresses = [ip.strip() for ip in response.text.split('\n') if ip.strip() and '.' in ip]
         create_dns_records(base_url, headers, ip_addresses, name_to_delete)
     else:
-        print("YMY无法获取反代IP地址信息。响应代码:", response.status_code)
+        print("无法获取IP信息。响应代码:", response.status_code)
         exit(1)
 
 if __name__ == "__main__":
