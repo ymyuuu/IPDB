eafa2071115bffb54251fbde132498f0e013f079
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-01-26 19:52:29 +0800

    Update bestcf.py

--- .github/workflows/bestcf.py
+++ .github/workflows/bestcf.py
@@ -2,21 +2,11 @@ import os
 import requests
 import re
 
-def get_a_records(dns_domain):
-    try:
-        return [record["rdata"] for record in requests.get(f"http://www.dns-lg.com/us01/{dns_domain}/a").json().get("answer", []) if record.get("type") == "A"]
-    except requests.exceptions.RequestException as e:
-        print(f"请求发生错误: {e}")
-        return []
-
-# 从环境变量获取多个 DNS 域名
-dns_domains = os.environ.get("NINE", "").split(",")
-# 从环境变量获取 Cloudflare API Token
+# Get GitHub Secrets from environment variables
 api_token = os.environ.get("CLOUDFLARE_API_TOKEN")
-# 从环境变量获取 Cloudflare Zone ID
 zone_id = os.environ.get("CLOUDFLARE_ZONE_ID")
-# 设置 DNS 记录的名称
-name = "9a"
+name = "bestcf"
+ipdb_api_url = "https://ipdb.api.030101.xyz/?type=bestcf"
 
 headers = {
     "Authorization": f"Bearer {api_token}",
@@ -38,31 +28,21 @@ def create_dns_record(ip):
     }
     requests.post(create_url, headers=headers, json=create_data)
 
-# 用于存储所有唯一 IP 地址的集合
-unique_ips = set()
+url = f"https://proxy.api.030101.xyz/https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
+response = requests.get(url, headers=headers)
+data = response.json()
 
-# 针对每个 DNS 域名执行相同的操作
-for dns_domain in dns_domains:
-    # 使用新的方式获取 IP
-    new_ip_list = get_a_records(dns_domain)
-
-    # 将新的 IP 地址添加到集合中
-    unique_ips.update(new_ip_list)
-
-    # 删除旧的 DNS 记录
-    url = f"https://proxy.api.030101.xyz/https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    for record in data["result"]:
-        record_name = record["name"]
-        if re.search(name, record_name):
-            delete_dns_record(record["id"])
+for record in data["result"]:
+    record_name = record["name"]
+    if re.search(name, record_name):
+        delete_dns_record(record["id"])
 
 print(f"\nSuccessfully delete records with name {name}, updating DNS records now")
 
-# 创建新的 DNS 记录，使用去重后的 IP 地址
-for new_ip in unique_ips:
+ipdb_response = requests.get(ipdb_api_url)
+new_ip_list = ipdb_response.text.strip().split("\n")
+
+for new_ip in new_ip_list:
     create_dns_record(new_ip)
 
-print(f"\nSuccessfully update {name} DNS records with unique IP addresses")
+print(f"\nSuccessfully update {name} DNS records")
