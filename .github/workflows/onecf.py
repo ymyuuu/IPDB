f954b4d7a2b8b517eb600623025914d121ae06fe
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-01-28 23:56:55 +0800

    Update onecf.py

--- .github/workflows/onecf.py
+++ .github/workflows/onecf.py
@@ -47,8 +47,8 @@ def update_dns_records(zone_id, name, dns_domains, headers, excluded_networks):
 if __name__ == "__main__":
     name = "@"
     dns_domains = os.environ.get("DOMAINS", "").split(",")
-    zone_id = os.environ.get("ONECF_CLOUDFLARE_ZONE_ID")
-    api_key = os.environ.get("ONECF_CLOUDFLARE_API_TOKEN")
+    zone_id = os.environ.get("CLOUDFLARE_ZONE_ID")
+    api_key = os.environ.get("CLOUDFLARE_API_TOKEN")
     excluded_networks = ["173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18",
                          "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22", "198.41.128.0/17",
                          "162.158.0.0/15", "104.16.0.0/13", "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22"]
