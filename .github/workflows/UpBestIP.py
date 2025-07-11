8014eccc84f5cae7fc4220bbccd8984bd5983713
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2025-07-11 10:26:17 +0800

    Update UpBestIP.py

--- .github/workflows/UpBestIP.py
+++ .github/workflows/UpBestIP.py
@@ -24,8 +24,8 @@ subdomain_configs = {
      "bestcfv6": {
         "v6": "https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestCF/bestcfv6.txt"
     },     
-     "bestali": {
-        "v4": "https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestAli/bestaliv4.txt"
+     "bestaedg": {
+        "v4": "https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestEDG/bestedgv4.txt"
     },
     # "bestgc": {
     #     "v4": "https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestGC/bestgcv4.txt",
